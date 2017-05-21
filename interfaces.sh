b09437e03509ad8f380c64c2cce01e95dc6ae4ef
Sebastian Martin Dicke <sebastian@sebastian-dicke.de>
2017-05-21 02:00:17 +0200

    added option to hide lo device in interfaces.sh

--- interfaces.sh
+++ interfaces.sh
@@ -2,11 +2,12 @@
 
 #Lists all network interfaces available on the system
 
+excludelo=$1;
 list=`ip address show | awk 'index($1, ":") != 0 {print substr($2, 0, index($2, ":") -1)}'`;
 
 for current in $list;
 do
-	if [ $current ]; then
+	if [[ $current && !($excludelo && $current = "lo") ]]; then
 		echo $current;
 	fi;
 done;
