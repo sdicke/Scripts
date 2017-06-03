496991ffd4a37991c2f6e1e54710bde2925c30d0
Sebastian Martin Dicke <sebastianmartindicke@gmx.de>
2017-06-03 22:57:16 +0200

    getcursors.sh added

---
+++ getcursors.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#fetches and installs Debian's oxgencursors on non-debian system
+
+package=oxygencursors_0.0.2012-06-kde4.8-2.1_all.deb;
+dir=deb;
+data=data.tar;
+
+wget https://ftp.de.debian.org/debian/pool/main/o/oxygencursors/$package;
+
+mkdir $dir;
+mv $package $dir;
+cd $dir;
+ar -x $package;
+unxz $data.xz;
+tar -xf $data;
+su -c "cp -R etc usr /";
+cd ..;
+rm -r $dir;
