343277bc18b3372eb0994ce3d20ee2fb97e16e00
Sebastian Martin Dicke <sebastianmartindicke@gmx.de>
2017-06-11 17:40:25 +0200

    showips massively extended

--- showips.sh
+++ showips.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-for current in `. interfaces.sh`; 
+for current in  $(interfaces.sh); 
 do
-	echo $current `./getip.sh -4 $current` `./getip.sh -6 $current`;
+	interface=$current;
+	if [ "$1" = "--help" ]; then
+        echo "showips shows available interfaces and their attached IP addresses:";
+        echo "Usage: showips [nonames] [IP version] [lo]";
+        echo "nomames: do not show interfaces names";
+        echo "without version: both IPv4 and IPv6 addresses";
+        echo "lo: do not show loop device's address";
+	fi;
+	if [ "$1" = "nonames" ]; then
+		interface="";
+		version="$2";
+	else
+		version="$1";
+	fi;
+	if [[ $version && ($version = "4" || $version = "6") ]]; then
+		vmode="true";
+	fi;
+	if [[ $current = "lo" && ("$1" = "lo" || "$2" = "lo" || "$3" = "lo") ]]; then
+		continue;
+	fi;
+	if [  $vmode ]; then
+		echo $interface $(getip.sh $version $current);
+	else
+		echo $interface $(getip.sh 4 $current) $(getip.sh 6 $current);
+	fi;
 done;
