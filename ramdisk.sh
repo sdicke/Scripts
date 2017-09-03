ec9a6fc2308c78fbddf08d7db60dc6f59dd03f66
Sebastian Martin Dicke <sdicke@users.noreply.github.com>
2017-09-03 05:04:49 +0200

    Update ramdisk.sh

--- ramdisk.sh
+++ ramdisk.sh
@@ -11,7 +11,7 @@ fi;
 
 #Selected executing user as path owner, when no was given
 if [ -z "$user" ]; then
-	user=$LOGNAME;
+	user="$LOGNAME";
 fi;
 
 #Use tmpfs und allow user to access the path fully
