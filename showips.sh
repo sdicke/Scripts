394a691f57aee81c74b59d56924bd36446867191
Sebastian Martin Dicke <sdicke@users.noreply.github.com>
2017-09-03 05:57:50 +0200

    Update showips.sh

--- showips.sh
+++ showips.sh
@@ -2,7 +2,7 @@
 
 for current in  $(interfaces.sh); 
 do
-	interface=$current;
+	interface="$current";
 	if [[ "$1" = "--help" ]]; then
         	echo "showips shows available interfaces and their attached IP addresses:";
         	echo "Usage: showips [nonames] [IP version] [lo]";
@@ -16,15 +16,15 @@ do
 		shift;
 	fi;
 	version="$1";
-	if [[ "$version" = "4" || $version = "6" ]]; then
+	if [[ "$version" = "4" || "$version" = "6" ]]; then
 		vmode="true";
 		shift;
 		next="$1";
 	fi;
-	if [[ $current = "lo" && "$next" = "lo" ]]; then
+	if [[ "$current" = "lo" && "$next" = "lo" ]]; then
 		continue;
 	fi;
-	if [[ $vmode ]]; then
+	if [[ "$vmode" ]]; then
 		echo "$interface" "$(getip.sh "$version" "$current")";
 	else
 		echo "$interface" "$(getip.sh 4 "$current") $(getip.sh 6 "$current")";
