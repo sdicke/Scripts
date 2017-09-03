dc8928c5bf5ba8ec032071e38620bb29bde117ad
Sebastian Martin Dicke <sdicke@users.noreply.github.com>
2017-09-03 05:13:39 +0200

    Unified parenthesis usage

--- showips.sh
+++ showips.sh
@@ -3,14 +3,14 @@
 for current in  $(interfaces.sh); 
 do
 	interface=$current;
-	if [ "$1" = "--help" ]; then
+	if [[ "$1" = "--help" ]]; then
         echo "showips shows available interfaces and their attached IP addresses:";
         echo "Usage: showips [nonames] [IP version] [lo]";
         echo "nomames: do not show interfaces names";
         echo "without version: both IPv4 and IPv6 addresses";
         echo "lo: do not show loop device's address";
 	fi;
-	if [ "$1" = "nonames" ]; then
+	if [[ "$1" = "nonames" ]]; then
 		interface="";
 		version="$2";
 	else
@@ -22,7 +22,7 @@ do
 	if [[ $current = "lo" && ("$1" = "lo" || "$2" = "lo" || "$3" = "lo") ]]; then
 		continue;
 	fi;
-	if [  $vmode ]; then
+	if [[ $vmode ]]; then
 		echo "$interface" "$(getip.sh "$version" "$current")";
 	else
 		echo "$interface" "$(getip.sh 4 "$current") $(getip.sh 6 "$current")";
