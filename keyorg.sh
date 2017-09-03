b95edf22365d6ce7344da8e5e4daaaca617f88ed
Sebastian Martin Dicke <Sebastianmartindicke [@] gmx . de>
2017-09-03 05:00:21 +0200

    Replaced subshell with build-in variable

--- keyorg.sh
+++ keyorg.sh
@@ -3,7 +3,7 @@
 #Invoke this script by an SSH key file name. If your key is named id_nas, then Invoke with ./keyorg.sh id_nas
 
 key="$1";
-folder="/home/$(whoami)/.ssh/";
+folder="/home/$LOGNAME/.ssh/";
 list=$(ssh-add -l | awk '{print $3}');
 file="$folder$key";
 for current in $list; do
